c25fce106b8dfcce611443e182cb0b04c8475383
KutovyiAO <Kutovyiao@Gmail.com>
2017-08-17 03:07:44 +0300

    Add entity for users, form and part of controller

---
+++ src/AppBundle/Controller/RegistrationController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antonkutovoj
+ * Date: 17.08.17
+ * Time: 03:03
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class RegistrationController extends Controller
+{
+
+}
